8ac60d61ee0febf8d209f3da9a0a77061bff5217
Sergey X <serhento@gmail.com>
2020-05-17 20:11:26 +0300

    Updates

---
+++ precache-manifest.02d8d45a7beeaecc4cd1a0f2dd48d133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836da4a5cd35a2e81ad3e257280e50c5",
+    "url": "/e_shop_react/index.html"
+  },
+  {
+    "revision": "699e1a79c72c41e2e299",
+    "url": "/e_shop_react/static/css/main.ffaf31d4.chunk.css"
+  },
+  {
+    "revision": "a0af417abca2999ea26f",
+    "url": "/e_shop_react/static/js/2.b4c62248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e_shop_react/static/js/2.b4c62248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699e1a79c72c41e2e299",
+    "url": "/e_shop_react/static/js/main.1735960b.chunk.js"
+  },
+  {
+    "revision": "e34f799c3824dafdb41f",
+    "url": "/e_shop_react/static/js/runtime-main.1a47b6c5.js"
+  }
+]);
